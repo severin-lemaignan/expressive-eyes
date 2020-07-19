74855e0d3e2bf96827469f2ab940ed9ac82d79e1
catherine <Catherine3.Chambers@live.uwe.ac.uk>
2020-07-19 18:03:20 +0100

    Deleted Test.py. Added "display all images" function. All eyes completed.

--- src/expressive_eyes.py
+++ src/expressive_eyes.py
@@ -14,6 +14,7 @@ import time
 root = tkinter.Tk()
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
+
 DEFAULT_EYE = {"CENTER_X": 0, "CENTER_Y": 0,
                "SCALE_X": 1.0, "SCALE_Y": 1.0,
                "ANGLE": 0.0,
@@ -91,24 +92,17 @@ Y = (0.0, 0.0, 1.0); ANGLE = (0.0, -60.0, 60.0); BEND = (0.0, 0.0, 1.0)
         return result
 
 
-# Smaller than the default eyes so that surprise can be properly expressed (doesn't really work)
 class NeutralFace(Face):
     """
     Function to produce the neutral face.
 
-    Standard face is NeutralFace(0.8, 0.8)
-
-    SCALE_X = (1.0, 1.0, 1.0); SCALE_Y = (1.0, 1.0, 1.0);
+    Standard face is NeutralFace()
     """
-    def __init__(self, scale_x=0.8, scale_y=0.8):
+    def __init__(self):
         super().__init__(
             {
-                "SCALE_X": scale_x,
-                "SCALE_Y": scale_y,
             },
             {
-                "SCALE_X": scale_x,
-                "SCALE_Y": scale_y,
             })
 
 
@@ -254,7 +248,7 @@ class PleadingFace(Face):
 
     UPPER_LID_ANGLE = (0.0, -60.0, 60.0); LOWER_LID_Y = (0.0, 0.0, 1.0)
     """
-    def __init__(self, upper_lid_angle=20.0, lower_lid_y=0.5):
+    def __init__(self, upper_lid_angle=30.0, lower_lid_y=0.5):
         super().__init__(
             {
              "UPPER_LID_ANGLE": upper_lid_angle,
@@ -267,7 +261,29 @@ class PleadingFace(Face):
 
 
 class VulnerableFace(Face):
-    pass
+    """
+    Function to produce a vulnerable face.
+
+    Standard face is VulnerableFace(0.3, 20.0, 0.5, 10.0)
+
+    UPPER_LID_Y = (0.0, 0.0, 1.0); UPPER_LID_ANGLE = (0.0, -60.0, 60.0);
+    LOWER_LID_Y = (0.0, 0.0, 1.0); LOWER_LID_ANGLE = (0.0, -60.0, 60.0);
+    """
+    def __init__(self, upper_lid_y=0.3, upper_lid_angle=20.0, lower_lid_y=0.5, lower_lid_angle=10.0):
+        super().__init__(
+            {
+                "UPPER_LID_Y": upper_lid_y,
+                "UPPER_LID_ANGLE": upper_lid_angle,
+                "LOWER_LID_Y": lower_lid_y,
+                "LOWER_LID_ANGLE": lower_lid_angle
+            },
+            {
+                "UPPER_LID_Y": upper_lid_y,
+                "UPPER_LID_ANGLE": -upper_lid_angle,
+                "LOWER_LID_Y": lower_lid_y,
+                "LOWER_LID_ANGLE": -lower_lid_angle
+            })
+
 
 
 class DespairFace(Face):
@@ -325,7 +341,7 @@ class DisappointedFace(Face):
     UPPER_LID_Y = (0.0, 0.0, 1.0); UPPER_LID_ANGLE = (0.0, -60.0, 60.0);
     LOWER_LID_Y = (0.0, 0.0, 1.0);
     """
-    def __init__(self, upper_lid_y=0.3, upper_lid_angle=5.0, lower_lid_y=0.4):
+    def __init__(self, upper_lid_y=0.3, upper_lid_angle=10.0, lower_lid_y=0.4):
         super().__init__(
             {
                 "UPPER_LID_Y": upper_lid_y,
@@ -340,7 +356,29 @@ class DisappointedFace(Face):
 
 
 class EmbarrassedFace(Face):
-    pass
+    """
+    Function to produce an embarrassed face.
+
+    Standard face is EmbarrassedFace(0.5, 10.0, 0.2, 0.1)
+
+    UPPER_LID_Y = (0.0, 0.0, 1.0); UPPER_LID_ANGLE = (0.0, -60.0, 60.0);
+    UPPER_LID_BEND = (0.0, 0.0, 1.0);
+    LOWER_LID_Y = (0.0, 0.0, 1.0);
+    """
+    def __init__(self, upper_lid_y=0.5, upper_lid_angle=10.0, upper_lid_bend=0.2, lower_lid_y=0.1):
+        super().__init__(
+            {
+             "UPPER_LID_Y": upper_lid_y,
+             "UPPER_LID_ANGLE": upper_lid_angle,
+             "UPPER_LID_BEND": upper_lid_bend,
+             "LOWER_LID_Y": lower_lid_y
+            },
+            {
+             "UPPER_LID_Y": upper_lid_y,
+             "UPPER_LID_ANGLE": -upper_lid_angle,
+             "UPPER_LID_BEND": upper_lid_bend,
+             "LOWER_LID_Y": lower_lid_y
+            })
 
 
 # Sub-face of disgust.
@@ -396,8 +434,8 @@ class AnnoyedFace(Face):
 
     RIGHT EYE PARAMETERS:
     UPPER_INNER_RADIUS_X = (0.5, 0.0, 1.0); UPPER_OUTER_RADIUS_X = (0.5, 0.0, 1.0);
-    UPPER_LID_Y: (0.0, 0.0, 1.0) = UPPER_LID_ANGLE = (0.0, -60.0, 60.0);
-    LOWER_LID_Y: (0.0, 0.0, 1.0) = LOWER_LID_ANGLE = (0.0, -60.0, 60.0)
+    UPPER_LID_Y: (0.0, 0.0, 1.0); UPPER_LID_ANGLE = (0.0, -60.0, 60.0);
+    LOWER_LID_Y: (0.0, 0.0, 1.0); LOWER_LID_ANGLE = (0.0, -60.0, 60.0)
     """
     def __init__(self, left_eye=(30.0, 0.3, 10.0), right_eye=(1.0, 1.0, 0.2, 30.0, 0.4, 5.0)):
         super().__init__(
@@ -552,15 +590,69 @@ class AsleepFace(Face):
 
 # Sub-faces of surprise.
 class ConfusedFace(Face):
-    pass
+    """
+    Function to produce a confused face.
+
+    Standard face is ConfusedFace(left_eye=(), right_eye=())
+
+    LEFT EYE PARAMETERS:
+    LOWER_LID_Y = (0.0, 0.0, 1.0); LOWER_LID_BEND = = (0.0, 0.0, 1.0);
+
+    RIGHT EYE PARAMETERS:
+    UPPER_LID_Y: (0.0, 0.0, 1.0); UPPER_LID_ANGLE = (0.0, -60.0, 60.0);
+    LOWER_LID_Y: (0.0, 0.0, 1.0); LOWER_LID_ANGLE = (0.0, -60.0, 60.0); LOWER_LID_BEND = = (0.0, 0.0, 1.0);
+    """
+    def __init__(self, left_eye=(0.2, 0.2), right_eye=(0.3, 10.0, 0.2, 5.0, 0.2)):
+        super().__init__(
+            {
+                "LOWER_LID_Y": left_eye[0],
+                "LOWER_LID_BEND": left_eye[1]
+            },
+            {
+                "UPPER_LID_Y": right_eye[0],
+                "UPPER_LID_ANGLE": -right_eye[1],
+                "LOWER_LID_Y": right_eye[2],
+                "LOWER_LID_ANGLE": right_eye[3],
+                "LOWER_LID_BEND": right_eye[4]
+            })
 
 
 class AmazedFace(Face):
-    pass
+    """
+    Function to produce an amazed face.
+
+    Standard face is AmazedFace()
+
+    LOWER_LID_Y = (0.0, 0.0, 1.0);
+    """
+    def __init__(self, lower_lid_y=0.2):
+        super().__init__(
+            {
+                "LOWER_LID_Y": lower_lid_y
+            },
+            {
+                "LOWER_LID_Y": lower_lid_y
+            })
 
 
 class ExcitedFace(Face):
-    pass
+    """
+    Function to produce an excited face.
+
+    Standard face is ExcitedFace()
+
+    LOWER_LID_Y = (0.0, 0.0, 1.0); LOWER_LID_BEND = (0.0, 0.0, 1.0);
+    """
+    def __init__(self, lower_lid_y=0.3, lower_lid_bend=0.2):
+        super().__init__(
+            {
+                "LOWER_LID_Y": lower_lid_y,
+                "LOWER_LID_BEND": lower_lid_bend
+            },
+            {
+                "LOWER_LID_Y": lower_lid_y,
+                "LOWER_LID_BEND": lower_lid_bend
+            })
 
 
 class BlinkLow(Face):
@@ -617,62 +709,61 @@ def interpolation(prior_face, next_face, alpha):
         large = cv2.resize(face_render, (width, height), interpolation=cv2.INTER_NEAREST)
         return cv2.imshow("Face", large)
 
-
 def get_next_frame(prior_face, interpolation_speed, event):
 
     int_speed = {
-        0: 0.1,  # NeutralFace(),
-        1: 0.1,  # HappyFace(),
-        2: 0.1,  # AmazedFace(),
-        3: 0.1,  # ExcitedFace(),
-        4: 0.1,  # AngryFace(),
-        5: 0.1,  # SurprisedFace(),
-        6: 0.1,  # FearFace(),
-        7: 0.1,  # DespairFace(),
-        8: 0.1,  # DisappointedFace(),
-        9: 0.1,  # EmbarrassedFace(),
-        10: 0.1,  # HorrifiedFace(),
-        11: 0.1,  # AnnoyedFace(),
-        12: 0.1,  # FuriousFace(),
-        13: 0.1,  # DisgustFace(),
-        14: 0.1,  # PleadingFace(),
-        15: 0.1,  # GuiltyFace(),
-        16: 0.1,  # SkepticalFace(),
-        17: 0.1,  # SuspiciousFace(),
-        18: 0.1,  # ConfusedFace(),
-        19: 0.1,  # SadFace(),
-        20: 0.1,  # VulnerableFace(),
-        21: 0.1,  # RejectedFace(),
-        22: 0.1,  # BoredFace(),
-        23: 0.1,  # TiredFace(),
-        24: 0.1  # AsleepFace()
+        0: 70,  # NeutralFace(),
+        1: 70,  # HappyFace(),
+        2: 70,  # AmazedFace(),
+        3: 70,  # ExcitedFace(),
+        4: 70,  # AngryFace(),
+        5: 70,  # SurprisedFace(),
+        6: 70,  # FearFace(),
+        7: 70,  # DespairFace(),
+        8: 70,  # DisappointedFace(),
+        9: 70,  # EmbarrassedFace(),
+        10: 70,  # HorrifiedFace(),
+        11: 70,  # AnnoyedFace(),
+        12: 70,  # FuriousFace(),
+        13: 70,  # DisgustFace(),
+        14: 70,  # PleadingFace(),
+        15: 70,  # GuiltyFace(),
+        16: 70,  # SkepticalFace(),
+        17: 70,  # SuspiciousFace(),
+        18: 70,  # ConfusedFace(),
+        19: 70,  # SadFace(),
+        20: 70,  # VulnerableFace(),
+        21: 70,  # RejectedFace(),
+        22: 70,  # BoredFace(),
+        23: 70,  # TiredFace(),
+        24: 70  # AsleepFace()
     }
     blink_height = {
         0: BlinkMed(),  # NeutralFace(),
-        1: BlinkMed(),  # HappyFace(),
+        1: BlinkHigh(),  # HappyFace(),
         2: BlinkMed(),  # AmazedFace(),
         3: BlinkMed(),  # ExcitedFace(),
-        4: BlinkMed(),  # AngryFace(),
+        4: BlinkLow(),  # AngryFace(),
         5: BlinkMed(),  # SurprisedFace(),
         6: BlinkMed(),  # FearFace(),
-        7: BlinkMed(),  # DespairFace(),
+        7: BlinkLow(),  # DespairFace(),
         8: BlinkMed(),  # DisappointedFace(),
-        9: BlinkMed(),  # EmbarrassedFace(),
+        9: BlinkLow(),  # EmbarrassedFace(),
         10: BlinkMed(),  # HorrifiedFace(),
         11: BlinkMed(),  # AnnoyedFace(),
         12: BlinkMed(),  # FuriousFace(),
         13: BlinkMed(),  # DisgustFace(),
         14: BlinkMed(),  # PleadingFace(),
-        15: BlinkMed(),  # GuiltyFace(),
-        16: BlinkMed(),  # SkepticalFace(),
+        15: BlinkLow(),  # GuiltyFace(),
+        16: BlinkLow(),  # SkepticalFace(),
         17: BlinkMed(),  # SuspiciousFace(),
         18: BlinkMed(),  # ConfusedFace(),
-        19: BlinkMed(),  # SadFace(),
+        19: BlinkLow(),  # SadFace(),
         20: BlinkMed(),  # VulnerableFace(),
-        21: BlinkMed(),  # RejectedFace(),
-        22: BlinkMed(),  # BoredFace(),
+        21: BlinkLow(),  # RejectedFace(),
+        22: BlinkLow(),  # BoredFace(),
         23: BlinkMed(),  # TiredFace(),
-        24: None  # AsleepFace()
+        24: BlinkLow()  # AsleepFace()
     }
     face_type = {
         0: NeutralFace(),
@@ -704,7 +795,33 @@ def get_next_frame(prior_face, interpolation_speed, event):
         22: BoredFace(),
         23: TiredFace(),
         24: AsleepFace()
-
+    }
+    blink_int_time = {
+        0: 6000,  # NeutralFace(),
+        1: 6000,  # HappyFace(),
+        2: 6000,  # AmazedFace(),
+        3: 6000,  # ExcitedFace(),
+        4: 6000,  # AngryFace(),
+        5: 6000,  # SurprisedFace(),
+        6: 6000,  # FearFace(),
+        7: 6000,  # DespairFace(),
+        8: 6000,  # DisappointedFace(),
+        9: 6000,  # EmbarrassedFace(),
+        10: 6000,  # HorrifiedFace(),
+        11: 6000,  # AnnoyedFace(),
+        12: 6000,  # FuriousFace(),
+        13: 6000,  # DisgustFace(),
+        14: 6000,  # PleadingFace(),
+        15: 6000,  # GuiltyFace(),
+        16: 6000,  # SkepticalFace(),
+        17: 6000,  # SuspiciousFace(),
+        18: 6000,  # ConfusedFace(),
+        19: 6000,  # SadFace(),
+        20: 6000,  # VulnerableFace(),
+        21: 6000,  # RejectedFace(),
+        22: 6000,  # BoredFace(),
+        23: 6000,  # TiredFace(),
+        24: 6000  # AsleepFace()
     }
 
     next_face = face_type[event]
@@ -713,20 +830,83 @@ def get_next_frame(prior_face, interpolation_speed, event):
         cv2.waitKey(100)
     interpolation_speed = int_speed[event]
     blink_height = blink_height[event]
+    blink_int_time = blink_int_time[event]
 
-    return next_face, interpolation_speed, blink_height
+    return next_face, interpolation_speed, blink_height, blink_int_time
 
+def display_all_faces():
+    face_type = {
+        0: NeutralFace(),
+        # High arousal high valence
+        1: HappyFace(),
+        2: AmazedFace(),
+        3: ExcitedFace(),
+        # High arousal low valence
+        4: AngryFace(),
+        5: SurprisedFace(),
+        6: FearFace(),
+        7: DespairFace(),
+        8: DisappointedFace(),
+        9: EmbarrassedFace(),
+        10: HorrifiedFace(),
+        11: AnnoyedFace(),
+        12: FuriousFace(),
+        # Med arousal low valence
+        13: DisgustFace(),
+        14: PleadingFace(),
+        15: GuiltyFace(),
+        16: SkepticalFace(),
+        17: SuspiciousFace(),
+        18: ConfusedFace(),
+        # Low arousal low valence
+        19: SadFace(),
+        20: VulnerableFace(),
+        21: RejectedFace(),
+        22: BoredFace(),
+        23: TiredFace(),
+        24: AsleepFace()
+    }
+    face_name = {
+        0: "Neutral",
+        1: "Happy",
+        2: "Amazed",
+        3: "Excited",
+        4: "Angry",
+        5: "Surprised",
+        6: "Fear",
+        7: "Despair",
+        8: "Disappointed",
+        9: "Embarrassed",
+        10: "Horrified",
+        11: "Annoyed",
+        12: "Furious",
+        13: "Disgust",
+        14: "Pleading",
+        15: "Guilty",
+        16: "Skeptical",
+        17: "Suspicious",
+        18: "Confused",
+        19: "Sad",
+        20: "Vulnerable",
+        21: "Rejected",
+        22: "Bored",
+        23: "Tired",
+        24: "Asleep"
+    }
+    face_render = []
+    for i in face_type.keys():
+        face_render.append(np.array(face_type[i].render()))
+        large = cv2.resize(face_render[i], (20, 20), interpolation=cv2.INTER_NEAREST)
+        cv2.imshow(face_name[i], large)
+        cv2.waitKey(100)
+        while i == 24:
+            wait_key = cv2.waitKey(0)
+            if wait_key == 27:
+                cv2.destroyAllWindows()
+                exit(0)
 
 def main():
-    face2 = NeutralFace()
-    face1 = BlinkHigh()
-    for alpha in np.arange(0, 1, 0.1):
-        face3 = face2.interpolateface(face1, alpha)
-        dst = cv2.medianBlur(np.array(face3.render()),5)
-        large = cv2.resize(dst, (width, height), interpolation=cv2.INTER_NEAREST)
-        cv2.imshow("image", large)
-        cv2.waitKey(100)
-
+    display_all_faces()
 
 
 if __name__ == '__main__':
