295e0200aa0991ac18249e8a3f56e589ffa2d8ac
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2020-07-23 14:39:55 +0100

    [sailfishos] Updated render.py to actually render the image

--- qml/render.py
+++ qml/render.py
@@ -16,7 +16,10 @@
 #
 
 import pyotherside
-import math
+
+from PIL import Image
+from PIL import ImageDraw
+
 
 def render(image_id, requested_size):
     print('image_id: "{image_id}", size: {requested_size}'.format(**locals()))
@@ -25,20 +28,14 @@ def render(image_id, requested_size):
     if requested_size == (-1, -1):
         requested_size = (300, 300)
 
-    width, height = requested_size
 
-    # center for circle
-    cx, cy = width/2, 10
+    filename = os.path.join(os.path.dirname(__file__), image_id)
+    img = Image.open(filename)
 
-    pixels = []
-    for y in range(height):
-        for x in range(width):
-            pixels.extend(reversed([
-                255, # alpha
-                int(10 + 10 * ((x - y * 0.5) % 20)), # red
-                20 + 10 * (y % 20), # green
-                int(255 * abs(math.sin(0.3*math.sqrt((cx-x)**2 + (cy-y)**2)))) # blue
-            ]))
-    return bytearray(pixels), (width, height), pyotherside.format_argb32
+    img.thumbnail(requested_size, Image.ANTIALIAS)
+
+    #b, g, r, a = img.split()
+    #img = Image.merge("RGBA", (r, g, b, a))
+    return bytearray(img.tobytes()), img.size, pyotherside.format_argb32
 
 pyotherside.set_image_provider(render)
