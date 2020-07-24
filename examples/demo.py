3c31bacc70dc021a12ac3bd17d3b4c1314e48130
catherine <Catherine3.Chambers@live.uwe.ac.uk>
2020-07-24 19:15:48 +0100

    Updated face manager class.

--- examples/demo.py
+++ examples/demo.py
@@ -1,18 +1,14 @@
-
 import cv2
 
-import FaceManager
+from face_manager import FaceManager
 
 
-fm = FaceManager(width = 600, height = 480)
-
+fm = FaceManager(width=600, height=480)
 
 while True:
 
-
     if cv2.waitKey(16) == 'a':
-        fm.show_expression("happy", duration = 4)
+        fm.show_expression("happy", duration=4)
 
-    face = fm.get_next_frame(delta = 16)
-    cv2.imshow(face)
-
+    face = fm.get_next_frame(delta=16, event=5)
+    cv2.imshow("Face", face)
--- src/expressive_eyes/face_manager.py
+++ src/expressive_eyes/face_manager.py
@@ -1,69 +1,71 @@
 from expressive_eyes import *
+import numpy as np
 
-key_event={
-    97: 1,
-    98: 2,
-    99: 3,
-    100: 4,
-    101: 5,
-    102: 6,
-    103: 7,
-    104: 8,
-    105: 9,
-    106: 10,
-    107: 11,
-    108: 12,
-    109: 13,
-    110: 14,
-    111: 15,
-    112: 16,
-    113: 17,
-    114: 18,
-    115: 19,
-    116: 20,
-    117: 21,
-    118: 22,
-    119: 23,
-    120: 24,
-    121: 0
-}
-
-delta = 6000 # 6 s blinking time
-Next_Face = NeutralFace()
-int_speed = 70
-blink_pos = BlinkMed()
-
-for alpha in np.arange(0, 1, 0.1):  # On startup
-    interpolation(BlinkMed(), NeutralFace(), alpha)
-    cv2.waitKey(int_speed+20)
-
-cv2.waitKey(4000)
-
-while True:
-    Prior_Face = Next_Face
-
-    for alpha in np.arange(0, 1, 0.1):  # Blink down
-        interpolation(Next_Face, blink_pos, alpha)
-        cv2.waitKey(int_speed)
-
-    for alpha in np.arange(0, 1, 0.1):  # Blink up
-        interpolation(blink_pos, Next_Face, alpha)
-        cv2.waitKey(int_speed)
-
-    key = cv2.waitKey(delta)
-
-    if key in range(97,122):  # a to y - all cv2.waitkey codes taken from http://www.asciitable.com/
-        Next_Face, int_speed, blink_pos, delta = get_next_frame(Prior_Face, 0.1, key_event[key])
-        cv2.waitKey(5000)
-    elif key == 27:  # ESC
-        cv2.destroyAllWindows()
-        exit(0)
 
 class FaceManager:
 
-    def __init__(self):
+    def __init__(self, width=600, height=480):
 
-        self.time = 0 # in ms
+        self.face_type = {
+            # Event: face, blink_height, blink_time, interpolation_speed, key_press
+            0: [NeutralFace(), BlinkMed(), 6000, 70, 121],
+            1: [HappyFace(), BlinkHigh(), 6000, 70, 97],
+            2: [AmazedFace(), BlinkMed(), 6000, 70, 98],
+            3: [ExcitedFace(), BlinkMed(), 6000, 70, 99],
+            4: [AngryFace(), BlinkLow(), 6000, 70, 100],
+            5: [SurprisedFace(), BlinkMed(), 6000, 70, 101],
+            6: [FearFace(), BlinkMed(), 6000, 70, 102],
+            7: [DespairFace(), BlinkLow(), 6000, 70, 103],
+            8: [DisappointedFace(), BlinkMed(), 6000, 70, 104],
+            9: [EmbarrassedFace(), BlinkLow(), 6000, 70, 105],
+            10: [HorrifiedFace(), BlinkMed(), 6000, 70, 106],
+            11: [AnnoyedFace(), BlinkMed(), 6000, 70, 107],
+            12: [FuriousFace(), BlinkMed(), 6000, 70, 108],
+            13: [DisgustFace(), BlinkMed(), 6000, 70, 109],
+            14: [PleadingFace(), BlinkMed(), 6000, 70, 110],
+            15: [GuiltyFace(), BlinkLow(), 6000, 70, 111],
+            16: [SkepticalFace(), BlinkLow(), 6000, 70, 112],
+            17: [SuspiciousFace(), BlinkMed(), 6000, 70, 113],
+            18: [ConfusedFace(),BlinkMed(), 6000, 70, 114],
+            19: [SadFace(), BlinkLow(), 6000, 70, 115],
+            20: [VulnerableFace(), BlinkMed(), 6000, 70, 116],
+            21: [RejectedFace(), BlinkLow(), 6000, 70, 117],
+            22: [BoredFace(), BlinkLow(), 6000, 70, 118],
+            23: [TiredFace(), BlinkLow(), 6000, 70, 119],
+            24: [AsleepFace(), BlinkMed(), 6000, 70, 120]
+        }
+        self.face_name = {
+            "Neutral": NeutralFace(),
+            "Happy": HappyFace(),
+            "Amazed": AmazedFace(),
+            "Excited": ExcitedFace(),
+            "Angry": AngryFace(),
+            "Surprised": SurprisedFace(),
+            "Fear": FearFace(),
+            "Despair": DespairFace(),
+            "Disappointed": DisappointedFace(),
+            "Embarrassed": EmbarrassedFace(),
+            "Horrified": HorrifiedFace(),
+            "Annoyed": AnnoyedFace(),
+            "Furious": FuriousFace(),
+            "Disgust": DisgustFace(),
+            "Pleading": PleadingFace(),
+            "Guilty": GuiltyFace(),
+            "Skeptical": SkepticalFace(),
+            "Suspicious": SuspiciousFace(),
+            "Confused": ConfusedFace(),
+            "Sad": SadFace(),
+            "Vulnerable": VulnerableFace(),
+            "Rejected": RejectedFace(),
+            "Bored": BoredFace(),
+            "Tired": TiredFace(),
+            "Asleep": AsleepFace()
+        }
+
+        self.width = width
+        self.height = height
+
+        self.time = 0  # in ms
 
         self.time_last_blink = 0
         self.is_blinking = False
@@ -73,29 +75,76 @@ class FaceManager:
         self.time_started_expression = 0
         self.is_showing_expression = False
 
-    def get_next_frame(self, delta):
+        self.prior_face = NeutralFace()
+        self.next_face = self.prior_face
+        self.blink_height = BlinkMed()
+        self.blink_time = 6000
+        self.interpolation_speed = 70
+
+    def get_next_frame(self, delta, event):
 
         self.time += delta
+        variables = self.face_type[event]
+        key = cv2.waitKey(100)
 
-        if self.time - self.time_last_blink > 6000:
+        if key == 27:  # ESC
+            cv2.destroyAllWindows()
+            exit(0)
+
+        if self.time - self.time_last_blink > 6000:  # Blink if time since last blink > 6000ms
+            for alpha in np.arange(0, 1, 0.1):
+                self.interpolation(self.next_face, self.blink_height, alpha)
+                cv2.waitKey(self.face_type[event[4]])
+
+            for alpha in np.arange(0, 1, 0.1):
+                self.interpolation(self.blink_height, self.next_face, alpha)
+                cv2.waitKey(self.face_type[event[4]])
+
             self.time_last_blink = self.time
             self.is_blinking = True
 
         if self.is_blinking:
-            blinking_time = self.time - self.time_last_blink
-            interpolateface(...)
-            ...
+            self.blink_time = self.time - self.time_last_blink
+
+        if self.next_expression is None:
+            variables = self.face_type[event]
+            self.next_face = variables[0]; self.blink_height = variables[1]
+            self.blink_time = variables[2]; self.interpolation_speed = variables[3]
+            self.prior_face = self.next_face
 
         if self.next_expression is not None and not self.is_showing_expression:
+            for alpha in np.arange(0, 1, 0.1):
+                self.interpolation(self.prior_face, self.next_face, alpha)
+                cv2.waitKey(self.face_type[event[4]])
+
             self.time_started_expression = self.time
             self.is_showing_expression = True
 
         if self.is_showing_expression:
-            ...
+            pass
 
+        return self.next_face
 
-        
-    def show_expression(type, duration):
-        self.next_expression = type
+    def interpolation(self, prior_face, next_face, alpha):
+        temp_face = prior_face.interpolateface(next_face, alpha)
+        face_render = np.array(temp_face.render())
+        large = cv2.resize(face_render, (self.width, self.height), interpolation=cv2.INTER_NEAREST)
+        return cv2.imshow("Face", large)
+
+    def show_expression(self, face_str, duration):
+        self.next_face = self.face_name[face_str]
         self.next_expression_duration = duration
 
+    def display_all_faces(self):
+        face_render = []
+        for name, face in self.face_name.items():
+            i = list(self.face_name.keys()).index(name)
+            face_render.append(np.array(face.render()))
+            large = cv2.resize(face_render[i], (200, 200), interpolation=cv2.INTER_NEAREST)
+            cv2.imshow(name, large)
+            cv2.waitKey(100)
+            while i == 24:
+                wait_key = cv2.waitKey(0)
+                if wait_key == 27:
+                    cv2.destroyAllWindows()
+                    exit(0)
