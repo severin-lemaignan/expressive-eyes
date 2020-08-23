80261036c6c9310758611b0e1287c3c2bc6a9258
catherine <Catherine3.Chambers@live.uwe.ac.uk>
2020-08-23 07:15:30 +0100

    Added elapsed time that uses time.time() for consistent times between frames.

--- examples/demo.py
+++ examples/demo.py
@@ -1,40 +1,40 @@
+from face_manager import FaceManager
 import cv2
+import time
 
-from expressive_eyes.face_manager import *
-
-fm = FaceManager(width=1980, height=1080)
-
+fm = FaceManager(1920, 1080)
 key_event = {
-    97: 1,
-    98: 2,
-    99: 3,
-    100: 4,
-    101: 5,
-    102: 6,
-    103: 7,
-    104: 8,
-    105: 9,
-    106: 10,
-    107: 11,
-    108: 12,
-    109: 13,
-    110: 14,
-    111: 15,
-    112: 16,
-    113: 17,
-    114: 18,
-    115: 19,
-    116: 20,
-    117: 21,
-    118: 22,
-    119: 23,
-    120: 24,
-    121: 0
+    97: "Neutral",
+    98: "Happy",
+    99: "Amazed",
+    100: "Excited",
+    101: "Angry",
+    102: "Surprised",
+    103: "Fear",
+    104: "Despair",
+    105: "Disappointed",
+    106: "Embarrassed",
+    107: "Horrified",
+    108: "Annoyed",
+    109: "Furious",
+    110: "Disgust",
+    111: "Pleading",
+    112: "Guilty",
+    113: "Skeptical",
+    114: "Suspicious",
+    115: "Confused",
+    116: "Sad",
+    117: "Vulnerable",
+    118: "Rejected",
+    119: "Bored",
+    120: "Tired",
+    121: "Asleep"
 }
-
-# fm.display_all_faces()
+elapsed_time = 0
 
 while True:
-    face, wait_time = fm.run_expressive_eyes(event=4, elapsed_time_since_last_call=1000)
-    cv2.waitKey(wait_time)
+    begin_time = time.time()
+    face = fm.run_expressive_eyes(face_str="Angry", elapsed_time=elapsed_time)
+    elapsed_time = time.time() - begin_time
+    cv2.waitKey(int(1000*elapsed_time))
     cv2.imshow("Face", face)
