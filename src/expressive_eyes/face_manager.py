6bbd960136fcfc3b73cced7c809796c9b380c0eb
catherine <Catherine3.Chambers@live.uwe.ac.uk>
2020-08-23 07:13:59 +0100

    Removed wait_time and other unnecessary timing variables. Changed next_face to current_face. Added eye_position and openface_implementation methods. Timing of interploation now consistent with different width and height - still not consistent with different elapsed time. Changed display_all_faces so that it works.

--- src/expressive_eyes/face_manager.py
+++ src/expressive_eyes/face_manager.py
@@ -1,145 +1,172 @@
-from .faces import *
+from faces import *
 import numpy as np
 
 
 class FaceManager:
 
     def __init__(self, width=128, height=64):
-
         self.face_type = {
-            # Event: face, blink_height, blink_time, interpolation_speed
-            0: [NeutralFace(), BlinkMed(), 6000, 4000],
-            1: [HappyFace(), BlinkHigh(), 6000, 4000],
-            2: [AmazedFace(), BlinkMed(), 6000, 4000],
-            3: [ExcitedFace(), BlinkMed(), 6000, 4000],
-            4: [AngryFace(), BlinkLow(), 6000, 4000],
-            5: [SurprisedFace(), BlinkMed(), 6000, 4000],
-            6: [FearFace(), BlinkMed(), 6000, 4000],
-            7: [DespairFace(), BlinkLow(), 6000, 4000],
-            8: [DisappointedFace(), BlinkMed(), 6000, 4000],
-            9: [EmbarrassedFace(), BlinkLow(), 6000, 4000],
-            10: [HorrifiedFace(), BlinkMed(), 6000, 4000],
-            11: [AnnoyedFace(), BlinkMed(), 6000, 4000],
-            12: [FuriousFace(), BlinkMed(), 6000, 4000],
-            13: [DisgustFace(), BlinkMed(), 6000, 4000],
-            14: [PleadingFace(), BlinkMed(), 6000, 4000],
-            15: [GuiltyFace(), BlinkLow(), 6000, 4000],
-            16: [SkepticalFace(), BlinkLow(), 6000, 4000],
-            17: [SuspiciousFace(), BlinkMed(), 6000, 4000],
-            18: [ConfusedFace(), BlinkMed(), 6000, 4000],
-            19: [SadFace(), BlinkLow(), 6000, 4000],
-            20: [VulnerableFace(), BlinkMed(), 6000, 4000],
-            21: [RejectedFace(), BlinkLow(), 6000, 4000],
-            22: [BoredFace(), BlinkLow(), 6000, 4000],
-            23: [TiredFace(), BlinkLow(), 6000, 4000],
-            24: [AsleepFace(), BlinkMed(), 6000, 4000]
-        }
-        self.face_name = {
-            "Neutral": NeutralFace(),
-            "Happy": HappyFace(),
-            "Amazed": AmazedFace(),
-            "Excited": ExcitedFace(),
-            "Angry": AngryFace(),
-            "Surprised": SurprisedFace(),
-            "Fear": FearFace(),
-            "Despair": DespairFace(),
-            "Disappointed": DisappointedFace(),
-            "Embarrassed": EmbarrassedFace(),
-            "Horrified": HorrifiedFace(),
-            "Annoyed": AnnoyedFace(),
-            "Furious": FuriousFace(),
-            "Disgust": DisgustFace(),
-            "Pleading": PleadingFace(),
-            "Guilty": GuiltyFace(),
-            "Skeptical": SkepticalFace(),
-            "Suspicious": SuspiciousFace(),
-            "Confused": ConfusedFace(),
-            "Sad": SadFace(),
-            "Vulnerable": VulnerableFace(),
-            "Rejected": RejectedFace(),
-            "Bored": BoredFace(),
-            "Tired": TiredFace(),
-            "Asleep": AsleepFace()
+            # face face_type, blink_height, blink_time, const_interpolation_time (seconds)
+            "Neutral": [NeutralFace(), BlinkMed(), 6, 1],
+            "Happy": [HappyFace(), BlinkHigh(), 6000, 4],
+            "Amazed": [AmazedFace(), BlinkMed(), 6000, 4],
+            "Excited": [ExcitedFace(), BlinkMed(), 6000, 4],
+            "Angry": [AngryFace(), BlinkLow(), 6, 1],
+            "Surprised": [SurprisedFace(), BlinkMed(), 6000, 4],
+            "Fear": [FearFace(), BlinkMed(), 6000, 4],
+            "Despair": [DespairFace(), BlinkLow(), 6000, 4],
+            "Disappointed": [DisappointedFace(), BlinkMed(), 6, 4],
+            "Embarrassed": [EmbarrassedFace(), BlinkLow(), 6000, 4],
+            "Horrified": [HorrifiedFace(), BlinkMed(), 6000, 4],
+            "Annoyed": [AnnoyedFace(), BlinkMed(), 6000, 4],
+            "Furious": [FuriousFace(), BlinkMed(), 6000, 4],
+            "Disgust": [DisgustFace(), BlinkMed(), 6000, 4],
+            "Pleading": [PleadingFace(), BlinkMed(), 6000, 4],
+            "Guilty": [GuiltyFace(), BlinkLow(), 6000, 4],
+            "Skeptical": [SkepticalFace(), BlinkLow(), 6000, 4],
+            "Suspicious": [SuspiciousFace(), BlinkMed(), 6000, 4],
+            "Confused": [ConfusedFace(), BlinkMed(), 6000, 4],
+            "Sad": [SadFace(), BlinkLow(), 6000, 4],
+            "Vulnerable": [VulnerableFace(), BlinkMed(), 6000, 4],
+            "Rejected": [RejectedFace(), BlinkLow(), 6000, 4],
+            "Bored": [BoredFace(), BlinkLow(), 6000, 4],
+            "Tired": [TiredFace(), BlinkLow(), 6000, 4],
+            "Asleep": [AsleepFace(), BlinkMed(), 6000, 4]
         }
 
         self.width = width
         self.height = height
 
-        self.time = 0
-        self.time_last_blink = 0
-        self.duration = 0
-
-        self.startup = False
+        self.startup = True
         self.blink_down = True
         self.next_expression = False
 
-        self.prior_face, self.blink_height, self.blink_time, self.interpolation_speed = self.face_type[0]
+        self.face_str = "Neutral"
+        self.prior_face, self.blink_height, self.blink_time, self.const_interpolation_time = self.face_type[self.face_str]
 
-        self.next_face = self.prior_face
+        self.current_face = self.prior_face
 
-        self.time_elapsed_since_beginning_of_interpolation = 0
         self.frames = 0
-        self.wait_time = 0
+        self.time_between_frames = 0
+        self.time_last_blink = 0
 
-    def run_expressive_eyes(self, event, elapsed_time_since_last_call):
-        self.wait_time = elapsed_time_since_last_call
-        self.duration = elapsed_time_since_last_call
-        self.time += self.duration
+    def run_expressive_eyes(self, face_str, elapsed_time):
+        self.time_between_frames = elapsed_time
 
-        if self.startup is False:  # Startup
-            face, self.wait_time, self.startup = self.get_next_frame(self.blink_time, self.startup, self.blink_height, self.next_face)
-            return face, self.wait_time
+        if self.startup:
+            face, self.startup = self.get_next_frame(self.startup, self.const_interpolation_time, self.blink_height,
+                                                     self.current_face)
+            return face
 
-        if self.time - self.time_last_blink > self.blink_time:  # Blinking
-            if self.blink_down:  # Blinking down
-                face, self.wait_time, self.blink_down = self.get_next_frame(self.blink_time, self.blink_down, self.next_face, self.blink_height)
-                return face, self.wait_time
-            else:  # Blinking up
-                face, self.wait_time, self.blink_down = self.get_next_frame(self.blink_time, self.blink_down, self.blink_height, self.next_face)
+        self.face_str = face_str
+        self.time_last_blink += elapsed_time
+
+        if self.time_last_blink > self.blink_time:
+            if self.blink_down:
+                face, self.blink_down = self.get_next_frame(self.blink_down, self.blink_time, self.current_face,
+                                                            self.blink_height)
+                return face
+            else:
+                face, self.blink_down = self.get_next_frame(self.blink_down, self.blink_time, self.blink_height,
+                                                            self.current_face)
                 if self.blink_down:
-                    self.time_last_blink = self.time
-                    return face, self.wait_time
+                    self.time_last_blink = 0
+                    return face
                 else:
-                    return face, self.wait_time
+                    return face
 
-        if self.next_expression is False:  # Setting the next expression
-            self.prior_face = self.next_face
-            self.next_face, self.blink_height, self.blink_time, self.interpolation_speed = self.face_type[event]
+        if self.next_expression is False:
+            self.prior_face = self.current_face
+            self.current_face, self.blink_height, self.blink_time, self.const_interpolation_time = self.face_type[self.face_str]
+            self.eye_position(0, 0, 0)  # This cannot be static due to dictionary mutability.
+            # It sets values for both prior and current face so doesn't interpolate properly if they are the same
+            # and the eye_position changes
+            self.openface_implementation(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
             self.next_expression = True
 
-        if self.next_expression:  # Displaying the next expression
-            if self.prior_face == self.next_face:
-                self.wait_time = 1
-                return self.interpolation(self.prior_face, self.next_face, 1.0, self.width, self.height), self.wait_time
+        if self.next_expression:
+            if self.prior_face == self.current_face:
+                return self.interpolation(self.prior_face, self.current_face, 1.0, self.width, self.height)
             else:
-                face, self.wait_time, self.next_expression = self.get_next_frame(self.interpolation_speed, self.next_expression, self.prior_face, self.next_face,)
-                return face, self.wait_time
+                face, self.next_expression = self.get_next_frame(self.next_expression, self.const_interpolation_time,
+                                                                 self.prior_face, self.current_face)
+                return face
 
-    def get_next_frame(self, interpolation_time, true_statement, prior_face, next_face):
-        self.frames = self.time_elapsed_since_beginning_of_interpolation/interpolation_time
-        if self.frames == 0:
-            self.wait_time = 1/(self.duration/interpolation_time)
+    def get_next_frame(self, boolean, interpolation_time, prior_face, current_face):
+        self.const_interpolation_time = interpolation_time
+        self.frames += self.time_between_frames / self.const_interpolation_time
+        if self.frames >= 1:
+            self.frames = 0
+            return self.interpolation(prior_face, current_face, 1.0, self.width, self.height), not boolean
         else:
-            self.wait_time = max(1, int(1/self.frames))
-        if self.frames > 1:
-            self.time_elapsed_since_beginning_of_interpolation = 0
-            return self.interpolation(prior_face, next_face, 1.0, self.width, self.height), self.wait_time, not true_statement
-        else:
-            self.time_elapsed_since_beginning_of_interpolation += self.duration
-            return self.interpolation(prior_face, next_face, self.frames, self.width, self.height), self.wait_time, true_statement
+            return self.interpolation(prior_face, current_face, self.frames, self.width, self.height), boolean
+
+    def openface_implementation(self, au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23,
+                                au25, au26, au45):
+        au_names = {
+            "outer brow raiser": au02,
+            "brow lowerer": au04,
+            "upper lid raiser": au05,
+            "cheek raiser": au06,
+            "lid tightener": au07,
+            "nose wrinkler": au09,
+            "upper lip raiser": au10,
+            "lip corner puller": au12,
+            "dimpler": au14,
+            "lip corner depresser": au15,
+            "chin raiser": au17,
+            "lip stretcher": au20,
+            "lip tightener": au23,
+            "lips part": au25,
+            "jaw drop": au26,
+            "blink": au45
+        }
+        au_standard_values = {
+            "Neutral": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Happy": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Amazed": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Excited": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Angry": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Surprised": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Fear": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Despair": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Disappointed": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Embarrassed": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Horrified": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Annoyed": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Furious": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Disgust": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Pleading": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Guilty": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Skeptical": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Suspicious": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Confused": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Sad": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Vulnerable": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Rejected": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Bored": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Tired": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45],
+            "Asleep": [au02, au04, au05, au06, au07, au09, au10, au12, au14, au15, au17, au20, au23, au25, au26, au45]
+        }
+        face_str = self.face_str
+        return face_str
+
+    def eye_position(self, eye_y, left_eye_x, right_eye_x):
+        self.current_face.left[0] = self.blink_height.left[0] = left_eye_x
+        self.current_face.right[0] = self.blink_height.right[0] = right_eye_x
+        self.current_face.left[1] = self.current_face.right[1] = \
+            self.blink_height.left[1] = self.blink_height.right[1] = eye_y
 
     @staticmethod
-    def interpolation(prior_face, next_face, alpha, width, height):
-        temp_face = prior_face.interpolateface(next_face, alpha)
+    def interpolation(prior_face, current_face, frames, width, height):
+        temp_face = prior_face.interpolateface(current_face, frames)
         face_render = np.array(temp_face.face_render(width=width, height=height))
         return face_render
 
-    def show_expression(self, face_str, duration):
-        self.next_face = self.face_name[face_str]
-        self.duration = duration
-
     def display_all_faces(self):
         face_show = []
-        for name, face in self.face_name.items():
-            face_show.append(np.array(face.face_render()))
-            cv2.imshow(name, face_show)
+        face = [item[0:25][0] for item in list(self.face_type.values())]
+        name = list(self.face_type.keys())
+        for i in range(0, 25):
+            face_show.append(np.array(face[i].face_render()))
+            cv2.imshow(name[i], face_show[i])
