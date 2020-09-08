7d15dcb8d2f0c538a51308b0793fe0f9d1e8be4a
catherine <Catherine3.Chambers@live.uwe.ac.uk>
2020-09-08 15:17:17 +0100

    Added comments for future users

--- examples/demo.py
+++ examples/demo.py
@@ -10,6 +10,9 @@ while True:
     begin_time = time.time()
     face = fm.run_expressive_eyes(face_str="Neutral", elapsed_time=elapsed_time)
     elapsed_time = time.time() - begin_time
+    # I am not sure about the following wait_time.
     wait_time = int(1000*elapsed_time/2)
+    # This waitKey is needed to properly display the interpolated face
     cv2.waitKey(wait_time)
+    # This is needed to display the face render
     cv2.imshow("Face", face)
